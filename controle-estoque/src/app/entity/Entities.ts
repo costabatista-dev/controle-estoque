6bb24f74854fc21f6ed88f5bfab36d104ea52b07
Paulo Batista <paulobatista@programmer.net>
2022-06-16 12:28:35 -0300

    adicionado tela de consulta de estoque

--- controle-estoque/src/app/entity/Entities.ts
+++ controle-estoque/src/app/entity/Entities.ts
@@ -29,11 +29,56 @@ export class Product extends Entity {
     price: number = 0;
 };
 
+export class Stock extends Entity {
+    private _quantity: number = 0;
+    private _unityValue: number = 0;
+    private _totalValue: number = 0;
+    private _department: string = '';
+
+    constructor(id: number, name: string) {
+        super(name);
+        this.id = id;
+    }
+
+    public set quantity(quantity: number) {
+        this._quantity = quantity;
+    }
+
+    public get quantity(): number {
+        return this._quantity;
+    }
+
+    public set unityValue(unityValue: number) {
+        this._unityValue = unityValue;
+    }
+
+    public get unityValue(): number {
+        return this._unityValue;
+    }
+
+    public set totalValue(totalValue: number) {
+        this._totalValue = totalValue;
+    }
+
+    public get totalValue(): number {
+        return this._totalValue;
+    }
+
+    public set department(department: string) {
+        this._department = this.department
+    }
+
+    public get department(): string {
+        return this._department;
+    }
+
+}
+
 export class Movement extends Entity {
     product: number;
-    locationToName:string = '';
-    locationFromName:string = '';
-    batchName:string = '';
+    locationToName: string = '';
+    locationFromName: string = '';
+    batchName: string = '';
     batch: number;
     type: string;
     quantity: number;
@@ -50,26 +95,26 @@ export class Movement extends Entity {
 }
 
 export class Quantity {
-    private _value:number;
+    private _value: number;
 
     constructor(value: number) {
         this._value = value;
     }
 
-    public get value():number {
+    public get value(): number {
         return this._value;
     }
 
-    public set value(value:number) {
+    public set value(value: number) {
         this._value = value;
     }
 }
 
 export class Sequence {
-    id:string;
-    nextVal:number;
+    id: string;
+    nextVal: number;
 
-    constructor(id:string, nextVal:number) {
+    constructor(id: string, nextVal: number) {
         this.id = id;
         this.nextVal = nextVal;
     }
